e4ef8c3c7f306fcd154d3df806b552a7894561e7
seitamuro <seitamuro0210@gmail.com>
2024-07-20 10:39:04 +0900

    lambdaにdynamodb streamを流す

--- lib/cdk-dynamodb-streams-stack.ts
+++ lib/cdk-dynamodb-streams-stack.ts
@@ -1,4 +1,7 @@
 import * as cdk from "aws-cdk-lib";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { DynamoEventSource } from "aws-cdk-lib/aws-lambda-event-sources";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
@@ -11,5 +14,19 @@ export class CdkDynamodbStreamsStack extends cdk.Stack {
       entry: "lambda/streaming_function.ts",
       handler: "handler",
     });
+
+    const table = new dynamodb.Table(this, "Table", {
+      partitionKey: { name: "id", type: dynamodb.AttributeType.STRING },
+      stream: dynamodb.StreamViewType.NEW_AND_OLD_IMAGES,
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
+
+    table.grantStreamRead(streaming_function);
+
+    streaming_function.addEventSource(
+      new DynamoEventSource(table, {
+        startingPosition: lambda.StartingPosition.TRIM_HORIZON,
+      })
+    );
   }
 }
